6acddfdc492f89600b4f44995ffc723e396ff58d
JeffreyzThomas <Jeffrey.thomas@hgs.hiddengeniusproject.org>
2026-07-09 13:46:25 -0700

    we have added a distance as a the score

--- Summegame#1/Assets/UIMAnager.cs
+++ Summegame#1/Assets/UIMAnager.cs
@@ -24,7 +24,7 @@ public class UIManager : MonoBehaviour
         }
     } 
 
-    public void UpdateScore(int score)
+    public void UpdateScore(int Distance)
     {
         // Update the score text object with the given score
         scoreText.text = $"Distance: {Distance}";
